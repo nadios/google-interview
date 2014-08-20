69a5159b16e4f53e8496b38313de6dd3937b9a03
Nadia Sokolova <nadezhda.sklv@gmail.com>
2014-08-20 21:40:23 +0300

    Task 2.1 done

--- src/com/nadia/crackcode/ch2/Task2_1.java
+++ src/com/nadia/crackcode/ch2/Task2_1.java
@@ -10,32 +10,37 @@ import com.nadia.crackcode.utils.lists.LinkedNode;
  */
 public class Task2_1 {
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         LinkedList list = new LinkedList();
         list.append(1);
-        list.append(2);
+//        list.append(2);
+//        list.append(3);
+//        list.append(4);
+//        list.append(7);
+//        list.append(5);
+        list.append(6);
+
         list.append(3);
-        list.append(4);
-        list.append(5);
         list.append(6);
-        list.append(6);
-        list.append(7);
+//        list.append(7);
         list.append(8);
-        list.append(5);
-        list.append(7);
+        list.append(2);
+        list.append(8);
+        list.append(0);
+        list.append(8);
         System.out.println("INIT: " + list.toString());
         remove_duplicates(list);
         System.out.println("PROCESSED: " + list.toString());
 
     }
 
-    public static void remove_duplicates(LinkedList list){
+    public static void remove_duplicates(LinkedList list) {
         LinkedNode current_node = list.getStart();
         while (current_node.getNext() != null) {
             Integer cur_value = current_node.getValue();
             LinkedNode node = current_node.getNext();
             LinkedNode prev = current_node;
-            while (node.getNext() != null) {
+            while (node != null) {
                 if (cur_value == node.getValue()) {
                     prev.setNext(node.getNext());
                 }
