2e46df100217e1b75128a7311bed1fa1edb0b10f
Nadia Sokolova <nadezhda.sklv@gmail.com>
2014-08-21 11:53:13 +0300

    Task 2.5 done

--- src/com/nadia/crackcode/ch2/Task2_5.java
+++ src/com/nadia/crackcode/ch2/Task2_5.java
@@ -1,5 +1,8 @@
 package com.nadia.crackcode.ch2;
 
+import com.nadia.crackcode.utils.lists.LinkedList;
+import com.nadia.crackcode.utils.lists.LinkedNode;
+
 /**
  * Given a circular linked list, implement an algorithm which returns node at the beginning of the loop
  * DEFINITION
@@ -8,7 +11,39 @@ package com.nadia.crackcode.ch2;
  * EXAMPLE
  * Input: A -> B -> C -> D -> E -> C [the same C as earlier]
  * Output: C
- * pg 50
  */
 public class Task2_5 {
+
+    public static void main(String args[]) {
+        LinkedList list1 = new LinkedList();
+        list1.append(1);
+        list1.append(2);
+        list1.append(3);
+        LinkedNode loopNode = list1.append(4);
+        list1.append(5);
+        list1.append(6);
+        list1.append(7);
+        LinkedNode lastNode = list1.append(8);
+        lastNode.setNext(loopNode);
+        list1.printLoop();
+        System.out.println("LOOP: " + findLoop(list1));
+    }
+
+    public static LinkedNode findLoop(LinkedList list) {
+        LinkedNode start1 = list.getStart();
+        LinkedNode start2 = list.getStart();
+        boolean isBegin = true;
+        while (start1 != start2 || isBegin) {
+            start1 = start1.getNext();
+            start2 = start2.getNext().getNext();
+            isBegin = false;
+        }
+        start1 = list.getStart();
+        while (start1 != start2) {
+            start1 = start1.getNext();
+            start2 = start2.getNext();
+        }
+        return start1;
+    }
+
 }
---
+++ src/com/nadia/crackcode/ch2/Task2_6.java
@@ -0,0 +1,7 @@
+package com.nadia.crackcode.ch2;
+
+/**
+ * Describe how you could use a single array to implement three stacks
+ */
+public class Task2_6 {
+}
