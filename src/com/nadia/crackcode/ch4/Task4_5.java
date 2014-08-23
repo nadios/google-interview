4f9f0ee97e662274a947f987f2fda2caf622b889
Nadia Sokolova <nadezhda.sklv@gmail.com>
2014-08-23 13:40:20 +0300

    Tasks 4.5 done

---
+++ src/com/nadia/crackcode/ch4/Task4_5.java
@@ -0,0 +1,77 @@
+package com.nadia.crackcode.ch4;
+
+import com.nadia.crackcode.utils.search.BiTreeNode;
+
+/**
+ * Write an algorithm to find the ‘next’ node (e g , in-order successor) of a given node in
+ * a binary search tree where each node has a link to its parent.
+ * ============================================================
+ * We approach this problem by thinking very, very carefully about what happens on an in-order traversal
+ * On an in-order traversal, we visit X left, then X, then X right
+ * So, if we want to find X successor(), we do the following:
+ * 1 If X has a right child, then the successor must be on the right side of X (because of the
+ * order in which we visit nodes) Specifically, the left-most child must be the first node visited
+ * in that subtree
+ * 2 Else, we go to X’s parent (call it P)
+ * 2 a If X was a left child (P left = X), then P is the successor of X
+ * 2 b If X was a right child (P right = X), then we have fully visited P, so we call successor(P)
+ */
+public class Task4_5 {
+
+    public static void main(String args[]) {
+        BiTreeNode vertexA = new BiTreeNode("A", null, null);
+        BiTreeNode vertexC = new BiTreeNode("C", null, null);
+        BiTreeNode vertexB = new BiTreeNode("B", vertexA, vertexC);
+        BiTreeNode vertexE = new BiTreeNode("E", null, null);
+        BiTreeNode vertexH = new BiTreeNode("H", null, null);
+        BiTreeNode vertexF = new BiTreeNode("F", vertexE, vertexH);
+        BiTreeNode vertexJ = new BiTreeNode("J", null, null);
+        BiTreeNode vertexL = new BiTreeNode("L", null, null);
+        BiTreeNode vertexK = new BiTreeNode("K", vertexJ, vertexL);
+        BiTreeNode vertexN = new BiTreeNode("N", null, null);
+        BiTreeNode vertexR = new BiTreeNode("R", null, null);
+        BiTreeNode vertexQ = new BiTreeNode("Q", vertexN, vertexR);
+        BiTreeNode vertexD = new BiTreeNode("D", vertexB, vertexF);
+        BiTreeNode vertexM = new BiTreeNode("M", vertexK, vertexQ);
+        BiTreeNode vertexI = new BiTreeNode("I", vertexD, vertexM);
+        BiTreeNode.print_line(vertexI);
+        //        _____I_____
+        //       /           \
+        //      D             M
+        //    /   \         /   \
+        //   B     F       K     Q
+        //  / \   / \     / \   /  \
+        // A   C E   H   J   L N    R
+        System.out.println("Successor of " + vertexI + " is " + findNext(vertexI));
+        System.out.println("Successor of " + vertexM + " is " + findNext(vertexM));
+        System.out.println("Successor of " + vertexL + " is " + findNext(vertexL));
+        System.out.println("Successor of " + vertexE + " is " + findNext(vertexE));
+        System.out.println("Successor of " + vertexH + " is " + findNext(vertexH));
+    }
+
+    public static BiTreeNode findNext(BiTreeNode current) {
+        if (current != null) {
+            BiTreeNode parent = current.getParent();
+            if (parent == null || current.getRight() != null) {
+                parent = findMostLeft(current.getRight());
+            } else {
+                while ((parent = current.getParent()) != null) {
+                    if (current == parent.getLeft()) {
+                        break;
+                    }
+                    current = parent;
+                }
+            }
+            return parent;
+        }
+        return null;
+    }
+
+    public static BiTreeNode findMostLeft(BiTreeNode vertex) {
+        if (vertex == null) return null;
+        while (vertex.getLeft() != null) vertex = vertex.getLeft();
+        return vertex;
+    }
+
+
+}
---
+++ src/com/nadia/crackcode/utils/search/BiTreeNode.java
@@ -0,0 +1,113 @@
+package com.nadia.crackcode.utils.search;
+
+import java.util.ArrayList;
+
+/**
+ * TreeNode Implementation
+ */
+public class BiTreeNode {
+
+    String name;
+    BiTreeNode left;
+    BiTreeNode right;
+    BiTreeNode parent;
+
+    public BiTreeNode(String name, BiTreeNode left, BiTreeNode right) {
+        this.name = name;
+        this.left = left;
+        this.right = right;
+        this.parent = null;
+        if (this.left != null) {
+            this.left.setParent(this);
+        }
+        if (this.right != null) {
+            this.right.setParent(this);
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BiTreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(BiTreeNode left) {
+        this.left = left;
+        this.left.setParent(this);
+    }
+
+    public BiTreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(BiTreeNode right) {
+        this.right = right;
+        this.right.setParent(this);
+    }
+
+    public BiTreeNode getParent() {
+        return parent;
+    }
+
+    public void setParent(BiTreeNode parent) {
+        this.parent = parent;
+    }
+
+    @Override
+    public String toString() {
+        return "'" + name + "\'   ";// (parent= " + this.getParent() + ")  ";
+    }
+
+    //        _____I_____
+    //       /           \
+    //      D             M
+    //    /   \         /   \
+    //   B     F       K     Q
+    //  / \   / \     / \   /  \
+    // A   C E   H   J   L N    R
+    public static BiTreeNode generateTreeI() {
+        BiTreeNode vertexA = new BiTreeNode("A", null, null);
+        BiTreeNode vertexC = new BiTreeNode("C", null, null);
+        BiTreeNode vertexB = new BiTreeNode("B", vertexA, vertexC);
+        BiTreeNode vertexE = new BiTreeNode("E", null, null);
+        BiTreeNode vertexH = new BiTreeNode("H", null, null);
+        BiTreeNode vertexF = new BiTreeNode("F", vertexE, vertexH);
+        BiTreeNode vertexJ = new BiTreeNode("J", null, null);
+        BiTreeNode vertexL = new BiTreeNode("L", null, null);
+        BiTreeNode vertexK = new BiTreeNode("K", vertexJ, vertexL);
+        BiTreeNode vertexN = new BiTreeNode("N", null, null);
+        BiTreeNode vertexR = new BiTreeNode("R", null, null);
+        BiTreeNode vertexQ = new BiTreeNode("Q", vertexN, vertexR);
+        BiTreeNode vertexD = new BiTreeNode("D", vertexB, vertexF);
+        BiTreeNode vertexM = new BiTreeNode("M", vertexK, vertexQ);
+        BiTreeNode vertexI = new BiTreeNode("I", vertexD, vertexM);
+        return vertexI;
+    }
+
+    public static void print_line(BiTreeNode vertex) {
+        ArrayList curLine = new ArrayList();
+        curLine.add(vertex);
+
+        while (curLine.size() > 0) {
+            ArrayList nextLine = new ArrayList();
+            for (int i = 0; i < curLine.size(); i++) {
+                BiTreeNode node = (BiTreeNode) curLine.get(i);
+                System.out.print(node);
+                if (node.getLeft() != null) {
+                    nextLine.add(node.getLeft());
+                }
+                if (node.getRight() != null) {
+                    nextLine.add(node.getRight());
+                }
+            }
+            System.out.println("");
+            curLine = nextLine;
+        }
+    }
+}
