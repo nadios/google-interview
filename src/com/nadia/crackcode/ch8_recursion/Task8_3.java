26a5cdc47f24e99d96650b9d9828baa9791f6722
Nadia Sokolova <nadezhda.sklv@gmail.com>
2014-08-24 20:17:08 +0300

    Tasks 8.3 done

--- src/com/nadia/crackcode/ch8_recursion/Task8_3.java
+++ src/com/nadia/crackcode/ch8_recursion/Task8_3.java
@@ -1,6 +1,7 @@
 package com.nadia.crackcode.ch8_recursion;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Write a method that returns all subsets of a set.
@@ -8,11 +9,20 @@ import java.util.ArrayList;
 public class Task8_3 {
 
     public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i = 0; i < 3; i++) {
+            list.add(i);
+        }
+        System.out.println("LIST: " + list.toString());
+        ArrayList<ArrayList<Integer>> subsets = getSubsets(list,0);//powerSet(list);
+        for (ArrayList subSet: subsets) {
+            System.out.println(subSet.toString());
+        }
     }
-
     public static ArrayList<ArrayList<Integer>> getSubsets(ArrayList<Integer> set, int index) {
         ArrayList<ArrayList<Integer>> allsubsets;
         if (set.size() == index) { // Base case - add empty set
+            System.out.println("Base case - add empty set : " + set.size() + " == " + index);
             allsubsets = new ArrayList<ArrayList<Integer>>();
             allsubsets.add(new ArrayList<Integer>());
         } else {
