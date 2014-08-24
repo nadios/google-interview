37eee2fe0fd20f810f0c7de3e99fdb53ec0948de
Nadia Sokolova <nadezhda.sklv@gmail.com>
2014-08-24 22:40:48 +0300

    Tasks 8.7 started

---
+++ src/com/nadia/crackcode/ch8_recursion/Task8_7.java
@@ -0,0 +1,13 @@
+package com.nadia.crackcode.ch8_recursion;
+
+/**
+ * Given an infinite number of quarters (25 cents), dimes (10 cents), nickels (5 cents) and
+ * pennies (1 cent), write code to calculate the number of ways of representing n cents
+ */
+public class Task8_7 {
+
+    public static void main (String args[]) {}
+
+    public static void calculate(int n_cents) {
+    }
+}
