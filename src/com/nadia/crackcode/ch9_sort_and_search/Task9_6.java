34bd0f9903ba0eaee09c2cd2aba656fed6b19668
Nadezhda Sokolova <nadezhda_sokolova@epam.com>
2014-08-25 18:13:00 +0300

    Task 9.6 initialized

---
+++ src/com/nadia/crackcode/ch9_sort_and_search/Task9_6.java
@@ -0,0 +1,13 @@
+package com.nadia.crackcode.ch9_sort_and_search;
+
+/**
+ * Given a matrix in which each row and each column is sorted, write a method to find an element in it
+ */
+public class Task9_6 {
+
+    public static void main(String[] args) {}
+
+    public static void findElement(Integer[][] matrix) {
+
+    }
+}
